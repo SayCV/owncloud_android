e3732916f2ab0a3b1c982d97c81be79cfa94c753
SayCV <sssxyc@163.com>
2013-02-28 00:32:25 +0800

    add utils_manual_cmd.bat

---
+++ utils_manual_cmd.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Copyright 2011 SayCV Inc. All rights reserved.
+echo Use of this source code is governed by a BSD-style
+echo license that can be found in the LICENSE file.
+echo.
+echo 2011 @ SayCV.Xiao
+echo.
+
+cd /d %~dp0
+set PATH=D:\MinGW\msys\1.0\bin;D:\MinGW\msys\1.0\local\bin;D:\MingW\bin;%PATH%
+
+set PATH=D:\cygwin\bin;%PATH%
+
+set HOME=%cd%
+
+
+bash setup_env.sh
+
+pause
+exit
